56de4be43ee239182f4b4a16b42992cbca5c8091
KaterinaKorobchits <katerina.korobchicz@mail.ru>
2019-06-22 00:36:36 +0300

    dz3 finished

--- app/src/main/java/by/itacademy/korobchits/dz3/Dz3Activity.kt
+++ app/src/main/java/by/itacademy/korobchits/dz3/Dz3Activity.kt
@@ -3,10 +3,20 @@ package by.itacademy.korobchits.dz3
 import android.app.Activity
 import android.os.Bundle
 import by.itacademy.korobchits.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_dz3.*
 
-class Dz3Activity : Activity(){
+class Dz3Activity : Activity() {
+    private val url = "https://99px.ru/sstorage/53/2013/06/tmb_72173_2040.jpg"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz3)
+
+        Glide.with(this@Dz3Activity)
+            .load(url)
+            .apply(RequestOptions().circleCrop())
+            .into(dz3Image)
     }
 }
